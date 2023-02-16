b7f1f9b82bb903762ba1557e08ae78200f52cb0a
Mitch Ludkey <mludk92@hotmail.com>
2023-02-15 20:26:12 -0600

    ran npm init and npm express

---
+++ server/server.js
@@ -0,0 +1,7 @@
+// express need to make config file run in terminal
+//  npm init --yes 
+//makes a package.json file (only run if package.json is not made)
+// if package.json is there then npm install --yes 
+// then run    npm install express 
+// creates node_modules ----not not modify this folder 
+
